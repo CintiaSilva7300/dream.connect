57026db50d5729a8073f154617d7640fd71b13c4
CINTIA SILVA <cintiasilva7300@gmail.com>
2023-10-02 10:12:25 -0300

    atualizar

--- src/utils/environments.js
+++ src/utils/environments.js
@@ -1,2 +1,2 @@
-// export const API_PROD = 'https://dream-connect-g2xg.vercel.app'
-export const API_PROD = 'http://localhost:4000';
+export const API_PROD = 'https://dream-connect-g2xg.vercel.app'
+// export const API_PROD = 'http://localhost:4000';
